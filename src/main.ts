956d3147809132024a4a2ae83023eac69f0da762
Jonata Serpa <jonataserpa@gmail.com>
2022-09-22 12:40:09 -0300

    adjust env port

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.use(helmet());
-  // app.setGlobalPrefix(process.env.API_PREFIX);
+  app.setGlobalPrefix(process.env.API_PREFIX);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors();
   
